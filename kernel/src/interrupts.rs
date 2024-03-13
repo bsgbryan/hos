675117416ce5574a97a4fd316363049bb4706d97
Bryan Maynard <bsgbryan@gmail.com>
2024-03-12 21:32:45 -0500

    First part of implementing CPU interrupts
    While this is working, there is no logging output; I haven't implemented that yet 😅

--- kernel/src/interrupts.rs
+++ kernel/src/interrupts.rs
@@ -3,9 +3,18 @@ use x86_64::structures::idt::{
     InterruptStackFrame,
 };
 
+use lazy_static::lazy_static;
+
+lazy_static! {
+    static ref IDT: InterruptDescriptorTable = {
+        let mut idt = InterruptDescriptorTable::new();
+        idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt
+    };
+}
+
 pub fn init_idt() {
-    let mut idt = InterruptDescriptorTable::new();
-    idt.breakpoint.set_handler_fn(breakpoint_handler);
+    IDT.load();
 }
 
 extern "x86-interrupt" fn breakpoint_handler(_stack_frame: InterruptStackFrame) {
--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -1,8 +1,12 @@
 #![feature(abi_x86_interrupt)]
 #![no_std]
 
-extern crate alloc;
+// extern crate alloc;
 
 pub mod framebuffer;
 pub mod interrupts;
 pub mod writer;
+
+pub fn init() {
+    interrupts::init_idt();
+}

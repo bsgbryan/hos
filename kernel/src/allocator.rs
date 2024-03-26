4c8a36237acb357c10d7cfd08a34e7612ef06755
Bryan Maynard <bsgbryan@gmail.com>
2024-03-26 00:23:53 -0500

    Implemented just the fixed_size_block allocator
    I only implemented the fixed size block allocator from the tutorial post.
    
    I am curious to see if/how I can use a bump allocator. I feel like some version of a slab allocator could work really well - especially considering that there shouldn't be(?) too many sizes of types Bevy instantiates.
    
    We'll see, though, maybe I'm all wet ...

--- kernel/src/allocator.rs
+++ kernel/src/allocator.rs
@@ -1,3 +1,5 @@
+pub mod fixed_size_block;
+
 use alloc::alloc::{
     GlobalAlloc,
     Layout,
@@ -5,7 +7,7 @@ use alloc::alloc::{
 
 use core::ptr::null_mut;
 
-use linked_list_allocator::LockedHeap;
+use fixed_size_block::FixedSizeBlockAllocator;
 
 use x86_64::{
     structures::paging::{
@@ -20,7 +22,7 @@ use x86_64::{
 };
 
 #[global_allocator]
-static ALLOCATOR: LockedHeap = LockedHeap::empty();
+static ALLOCATOR: Locked<FixedSizeBlockAllocator> = Locked::new(FixedSizeBlockAllocator::new());
 pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 100 * 1024; // 100 KiB
 
@@ -63,4 +65,28 @@ pub fn init_heap(
     }
 
     Ok(())
+}
+
+// /// Align the given address `addr` upwards to alignment `align`.
+// ///
+// /// Requires that `align` is a power of two.
+// fn align_up(addr: usize, align: usize) -> usize {
+//     (addr + align - 1) & !(align - 1)
+// }
+
+/// A wrapper around spin::Mutex to permit trait implementations.
+pub struct Locked<A> {
+    inner: spin::Mutex<A>,
+}
+
+impl<A> Locked<A> {
+    pub const fn new(inner: A) -> Self {
+        Locked {
+            inner: spin::Mutex::new(inner),
+        }
+    }
+
+    pub fn lock(&self) -> spin::MutexGuard<A> {
+        self.inner.lock()
+    }
 }
--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -1,6 +1,8 @@
-#![feature(abi_x86_interrupt)]
-#![feature(type_alias_impl_trait)]
 #![no_std]
+
+#![feature(abi_x86_interrupt)]
+#![feature(const_mut_refs)]
+#![feature(type_alias_impl_trait)]
 
 extern crate alloc;
 
